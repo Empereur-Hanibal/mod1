6a749b8286093c5ba35dba052a4cb71974e3fb5c
Hugman <contact.hugman@gmail.com>
2021-02-22 00:00:21 +0100

    j'ai fait du tri

---
+++ src/main/java/com/hanibal/mod1/Mod1.java
@@ -0,0 +1,10 @@
+package com.hanibal.mod1;
+
+import net.fabricmc.api.ModInitializer;
+
+public class Mod1 implements ModInitializer {
+	@Override
+	public void onInitialize() {
+		System.out.println("Hello Fabric world!");
+	}
+}
