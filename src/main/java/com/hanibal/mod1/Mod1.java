57da81ff04643666842820efef4966208f4efe53
Empereur Hanibal <louis.hagues@gmail.com>
2021-03-02 18:40:55 +0100

    4

--- src/main/java/com/hanibal/mod1/Mod1.java
+++ src/main/java/com/hanibal/mod1/Mod1.java
@@ -1,6 +1,5 @@
 package com.hanibal.mod1;
 
-import com.hanibal.mod1.objects.block.TinyPostBlock;
 import net.fabricmc.api.ModInitializer;
 import net.fabricmc.fabric.api.item.v1.FabricItemSettings;
 import net.fabricmc.fabric.api.object.builder.v1.block.FabricBlockSettings;
@@ -17,8 +16,8 @@ public class Mod1 implements ModInitializer {
 
 	// On initialise une variable pour le nouveau bloc. Il n'existe pas encore aux yeux de Minecraft, c'est uniquement une variable Block avec des données à l'intérieur, rien de plus.
 	public static final Block ANGELIC_BRICK = new Block(FabricBlockSettings.of(Material.STONE).sounds(BlockSoundGroup.STONE).strength(4.0f));
-	public static final Block ANGELIC_BRICK_POST = new TinyPostBlock(FabricBlockSettings.of(Material.STONE).sounds(BlockSoundGroup.STONE).strength(4.0f));
 	public static final Block ANGELIC_STONE = new Block (FabricBlockSettings.of(Material.STONE).sounds(BlockSoundGroup.STONE).strength(4.0f));
+	public static final Block ANGELIC_BOARD = new Block (FabricBlockSettings.of(Material.WOOD).sounds(BlockSoundGroup.WOOD).strength(4.0f));
 
 	@Override
 	public void onInitialize() {
@@ -28,13 +27,13 @@ public class Mod1 implements ModInitializer {
 		// On dit au jeu de créer un bloc (avec un certain indentifiant) qui correspond à la variable créée plus tôt
 		Registry.register(Registry.BLOCK, new Identifier("mod1", "angelic_brick"), ANGELIC_BRICK);
 		Registry.register(Registry.BLOCK, new Identifier("mod1", "angelic_stone"), ANGELIC_STONE);
-		Registry.register(Registry.BLOCK, new Identifier("mod1", "angelic_brick_post"), ANGELIC_BRICK_POST);
+		Registry.register(Registry.BLOCK, new Identifier("mod1", "angelic_board"), ANGELIC_BOARD);
 
 		// On dit au jeu de créer un objet qui correspondra au bloc (avec le même identifiant que le bloc; même si ce n'est pas obligatoire, c'est toujours mieux !)
 		// Ici, la classe BlockItem est une extension de la classe Item. Cette classe est très utilisée dans le jeu de base (évidemment...) et permet juste d'avoir le bloc dans notre inventaire et de le placer dans le monde.
 		Registry.register(Registry.ITEM, new Identifier("mod1", "angelic_brick"), new BlockItem(ANGELIC_BRICK, new FabricItemSettings().group(ItemGroup.BUILDING_BLOCKS)));
-		Registry.register(Registry.ITEM, new Identifier("mod1", "angelic_brick_post"), new BlockItem(ANGELIC_BRICK_POST, new FabricItemSettings().group(ItemGroup.BUILDING_BLOCKS)));
 		Registry.register(Registry.ITEM, new Identifier("mod1", "angelic_stone"), new BlockItem(ANGELIC_STONE, new FabricItemSettings().group(ItemGroup.BUILDING_BLOCKS)));
+		Registry.register(Registry.ITEM, new Identifier("mod1", "angelic_stone"), new BlockItem(ANGELIC_BOARD, new FabricItemSettings().group(ItemGroup.BUILDING_BLOCKS)));
 
 		/**
 		 * Les identifiants (ou ID) permettent au jeu de comprendre à quel bloc on a à faire
