37b8662a784641c8c5d37d1b5be249faa2a13f03
Empereur Hanibal <louis.hagues@gmail.com>
2021-03-03 18:09:33 +0100

    5

--- src/main/java/com/hanibal/mod1/Mod1.java
+++ src/main/java/com/hanibal/mod1/Mod1.java
@@ -17,7 +17,9 @@ public class Mod1 implements ModInitializer {
 	// On initialise une variable pour le nouveau bloc. Il n'existe pas encore aux yeux de Minecraft, c'est uniquement une variable Block avec des données à l'intérieur, rien de plus.
 	public static final Block ANGELIC_BRICK = new Block(FabricBlockSettings.of(Material.STONE).sounds(BlockSoundGroup.STONE).strength(4.0f));
 	public static final Block ANGELIC_STONE = new Block (FabricBlockSettings.of(Material.STONE).sounds(BlockSoundGroup.STONE).strength(4.0f));
+
 	public static final Block ANGELIC_BOARD = new Block (FabricBlockSettings.of(Material.WOOD).sounds(BlockSoundGroup.WOOD).strength(4.0f));
+	public static final Block ANGELIC_WOOD = new Block (FabricBlockSettings.of(Material.WOOD).sounds(BlockSoundGroup.WOOD).strength(4.0f));
 
 	@Override
 	public void onInitialize() {
@@ -27,18 +29,17 @@ public class Mod1 implements ModInitializer {
 		// On dit au jeu de créer un bloc (avec un certain indentifiant) qui correspond à la variable créée plus tôt
 		Registry.register(Registry.BLOCK, new Identifier("mod1", "angelic_brick"), ANGELIC_BRICK);
 		Registry.register(Registry.BLOCK, new Identifier("mod1", "angelic_stone"), ANGELIC_STONE);
+
 		Registry.register(Registry.BLOCK, new Identifier("mod1", "angelic_board"), ANGELIC_BOARD);
+		Registry.register(Registry.BLOCK, new Identifier("mod1", "angelic_wood"), ANGELIC_WOOD);
 
 		// On dit au jeu de créer un objet qui correspondra au bloc (avec le même identifiant que le bloc; même si ce n'est pas obligatoire, c'est toujours mieux !)
 		// Ici, la classe BlockItem est une extension de la classe Item. Cette classe est très utilisée dans le jeu de base (évidemment...) et permet juste d'avoir le bloc dans notre inventaire et de le placer dans le monde.
 		Registry.register(Registry.ITEM, new Identifier("mod1", "angelic_brick"), new BlockItem(ANGELIC_BRICK, new FabricItemSettings().group(ItemGroup.BUILDING_BLOCKS)));
 		Registry.register(Registry.ITEM, new Identifier("mod1", "angelic_stone"), new BlockItem(ANGELIC_STONE, new FabricItemSettings().group(ItemGroup.BUILDING_BLOCKS)));
-		Registry.register(Registry.ITEM, new Identifier("mod1", "angelic_stone"), new BlockItem(ANGELIC_BOARD, new FabricItemSettings().group(ItemGroup.BUILDING_BLOCKS)));
 
-		/**
-		 * Les identifiants (ou ID) permettent au jeu de comprendre à quel bloc on a à faire
-		 * Par exemple, cet ID sera utilisé pour sauvegarder les données des mondes, des tronçons, des inventaires
-		 * Il sert aussi au jeu de retrouver certaines données (visuelles par exemple), comme celles créées par toi dans les ressources du mod
-		 */
+		Registry.register(Registry.ITEM, new Identifier("mod1", "angelic_stone"), new BlockItem(ANGELIC_BOARD, new FabricItemSettings().group(ItemGroup.BUILDING_BLOCKS)));
+		Registry.register(Registry.ITEM, new Identifier("mod1", "angelic_stone"), new BlockItem(ANGELIC_WOOD, new FabricItemSettings().group(ItemGroup.BUILDING_BLOCKS)));
+
 	}
 }
